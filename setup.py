92b0cd49e4ac6c3def2357a56bcdf269a0416fb9
Jannis Konrad <kabel42@gmail.com>
2017-04-25 20:00:51 +0200

    init standalone wrapper

---
+++ setup.py
@@ -0,0 +1,41 @@
+import subprocess
+
+from distutils.core import setup
+from distutils.extension import Extension
+from distutils.command.build_ext import build_ext
+from Cython.Build import cythonize
+
+class prepare_tinydtls(build_ext):
+    def run(self):
+        def run_command(args):
+            print("Running:", " ".join(args))
+            subprocess.check_call(args, cwd="./tinydtls")
+        commands = [
+            ["autoreconf", "-i"],
+            ["./configure", "--without-ecc"],
+            ]
+        for command in commands:
+            run_command(command)
+        build_ext.run(self)
+
+cy_build = cythonize([
+      Extension("dtls", 
+                ["dtls.pyx", "tinydtls/dtls.c", "tinydtls/crypto.c", "tinydtls/ccm.c",
+                 "tinydtls/hmac.c", "tinydtls/netq.c", "tinydtls/peer.c", "tinydtls/dtls_time.c",
+                 "tinydtls/session.c", "tinydtls/dtls_debug.c",
+                 "tinydtls/aes/rijndael.c", "tinydtls/sha2/sha2.c"],
+                include_dirs=['tinydtls'],
+                define_macros=[('DTLSv12', '1'),
+                               ('WITH_SHA256', '1'),
+                               ('DTLS_CHECK_CONTENTTYPE', '1'),
+                               ('_GNU_SOURCE', '1')],
+                undef_macros = [ "NDEBUG" ],
+                )])
+
+setup(
+    name="DTLSSocket",
+    description = "DTLSSocket is a cython wrapper for tinydtls with a Socket like interface",
+    py_modules  = [ "DTLSSocket", ],
+    cmdclass    = {"build_ext": prepare_tinydtls},
+    ext_modules = cy_build,
+    )
