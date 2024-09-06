5a0f6b4c5286c4c4c6eafba3ba9a9add495d201d
Jannis Konrad <kabel42@gmail.com>
2024-09-06 11:25:04 +0200

    add tinydtls.pc.in to manifest

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="DTLSSocket",
-    version='0.2.1',
+    version='0.2.2',
     description = "DTLSSocket is a cython wrapper for tinydtls with a Socket like interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
